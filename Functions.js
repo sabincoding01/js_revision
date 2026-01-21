c0a88ee4261a64db2b5633d2f38b7b53c0984d5b
sabincoding01 <sabintimalsina74@gmail.com>
2026-01-21 19:05:37 +0545

    Function

---
+++ Functions.js
@@ -0,0 +1,9 @@
+//it is a reusable code block which is used to perform specific task. we can re use one function for many times
+
+function Name(surname){
+    console.log("sabin" + surname);
+}
+
+Name(' Timalsina');
+Name(' bajagain');
+
