a7b3ac401da15e322551dcfcf87b4d4e28ca0483
sabincoding01 <sabintimalsina74@gmail.com>
2026-01-22 20:42:13 +0545

    function cover

--- higherorder_function.js
+++ higherorder_function.js
@@ -10,3 +10,19 @@ return c-d;
 }
 
 console.log(fun(2,4));
+
+let Add= function(e,f){
+    return e+f;
+}
+
+let Sub = function (e,f){
+    return e-f;
+}
+
+let Func = function(operator , e, f){
+    return operator(e,f);
+
+}
+
+console.log(Func(Add,2,3));
+console.log(Func(Sub, 3,2));
