298c75da3faf27c3014812c4fc99f441d7774978
sabincoding01 <sabintimalsina74@gmail.com>
2026-01-22 20:23:02 +0545

    higherorder function

---
+++ higherorder_function.js
@@ -0,0 +1,12 @@
+// passing the value from one function to another
+function sum(a,b){
+     return a+b;
+}
+
+console.log(sum(2,5));
+
+let fun = function(c,d){
+return c-d;
+}
+
+console.log(fun(2,4));
